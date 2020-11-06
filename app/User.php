09ebd11a55457eda9432ed6c7b487de0bac83cff
Andrei <andrei_xavier@outlook.com>
2020-11-06 10:24:01 -0300

    password property included

--- app/User.php
+++ app/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $table = 'users';
-    protected $fillable = ['id', 'username', 'email', 'active', 'person_id'];
+    protected $fillable = ['id', 'username', 'email', 'active', 'password', 'person_id'];
+    protected $hidden = ['password'];
     public $timestamps = false;
 }
